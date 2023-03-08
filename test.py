1f52a19fbaa5458f3854d3e4bdb0d114fbcb45d8
j254liu <j254liu@uwaterloo.com>
2023-03-08 08:33:22 -0500

    debug

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import sys
 
 
 
-fd = os.open( "/tmp/j254liu/mount/touchfile.txt", os.O_RDWR )
+fd = os.open( "/tmp/j254liu/mount/newfile.txt", os.O_RDWR)
 
 
-l = os.write( fd, "happy" )
+# l = os.write( fd, "happy" )
 
 
-os.close( fd )
+# os.close( fd )
--- watdfs_server.d
+++ watdfs_server.d
@@ -1,3 +1,4 @@
 watdfs_server.o: watdfs_server.cpp rpc.h debug.h /usr/include/fuse/fuse.h \
  /usr/include/fuse/fuse_common.h /usr/include/fuse/fuse_opt.h \
- /usr/include/fuse/fuse_common_compat.h /usr/include/fuse/fuse_compat.h
+ /usr/include/fuse/fuse_common_compat.h /usr/include/fuse/fuse_compat.h \
+ rw_lock.h
