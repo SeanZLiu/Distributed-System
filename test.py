e3e9b619dde468e2e576f26f1832c89428140001
j254liu <j254liu@uwaterloo.com>
2023-03-08 23:45:15 -0500

    just debug

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ import sys
 
 # fd = os.open( "/tmp/j254liu/mount/newfile1.txt", os.O_RDWR)
 
-fd = os.open( "/tmp/j254liu/mount/newfile7.txt", os.O_RDWR| os.O_CREAT)
+fd = os.open( "/tmp/j254liu/mount/newfile8.txt", os.O_RDWR| os.O_CREAT)
 
 l = os.write( fd, "happy".encode())
-os.fsync(fd)
 
 
-# os.close( fd )
+
+os.close( fd )
